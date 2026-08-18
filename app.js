808a58a5bbf7f8288b5399161bdc2b9e595d8378
이루 <riruyaaa@gmail.com>
2026-08-19 01:58:31 +0900

    Auto-save

--- app.js
+++ app.js
@@ -28,6 +28,15 @@ if (applyForm) {
       if (res.ok) {
         applyForm.style.display = 'none';
         document.getElementById('formSuccess').style.display = 'block';
+
+        const introTag = document.querySelector('.apply-intro .tag');
+        const introHeading = document.querySelector('.apply-intro h2');
+        const introLead = document.querySelector('.apply-intro .lead');
+        if (introTag) introTag.style.display = 'none';
+        if (introHeading) introHeading.style.display = 'none';
+        if (introLead) {
+          introLead.innerHTML = '협업 제안을 보내주셔서 감사합니다.<br>남겨주신 내용을 꼼꼼히 확인한 뒤,<br>기재해주신 연락처로 안내드릴게요.';
+        }
       } else {
         throw new Error();
       }
